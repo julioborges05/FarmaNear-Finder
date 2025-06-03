a4ade220e2518702090e08ac702123e2de37bbc8
Júlio Borges <julioborges05@hotmail.com>
2025-06-03 18:15:06 -0300

    Adds project files

---
+++ src/main/java/br/com/fiap/FarmaNear_Finder/FarmaNearFinderApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.FarmaNear_Finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaNearFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaNearFinderApplication.class, args);
+	}
+
+}
