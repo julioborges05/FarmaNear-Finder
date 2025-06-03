ca5586fb68adeea15b694ae7103e8affba0666df
Júlio Borges <julioborges05@hotmail.com>
2025-06-03 19:10:26 -0300

    Adds a Google maps finder

---
+++ src/main/java/br/com/fiap/FarmaNear_Finder/service/GoogleMapsService.java
@@ -0,0 +1,35 @@
+package br.com.fiap.FarmaNear_Finder.service;
+
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@Service
+public class GoogleMapsService {
+
+    private final GeoApiContext context;
+
+    @Autowired
+    public GoogleMapsService(@Value("${maps.google.apikey}") String apiKey){
+        context = new GeoApiContext.Builder()
+                .apiKey(apiKey)
+                .build();
+    }
+
+    public LatLng getLatLngFromAddress(String address) throws InterruptedException, ApiException, IOException {
+        GeocodingResult[] results = GeocodingApi.geocode(context, address).await();
+        if (Objects.nonNull(results) && results.length > 0) {
+            return results[0].geometry.location;
+        }
+        return null;
+    }
+
+}
